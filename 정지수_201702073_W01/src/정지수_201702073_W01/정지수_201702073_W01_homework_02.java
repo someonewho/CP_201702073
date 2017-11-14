46dee87d065d4f4ff92d8877452bd1433d89c3f7
someonewho <bluedao2@naver.com>
2017-11-14 22:16:06 +0900

    HW01

---
+++ 정지수_201702073_W01/src/정지수_201702073_W01/정지수_201702073_W01_homework_02.java
@@ -0,0 +1,38 @@
+package 정지수_201702073_W01;
+
+public class 정지수_201702073_W01_homework_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int linenumber;
+		linenumber=1;
+     System.out.println(linenumber+"나는 동일한 길을 걷고 있었네.");
+        linenumber=2;
+     System.out.println(linenumber+"보도에는 깊은 구멍이 있었지.");
+        linenumber=3;
+     System.out.println(linenumber+"나는 그것을 보았네.");
+        linenumber=4;
+     System.out.println(linenumber+"나는 여전히 거기에 빠졌지, 습관적으로.");
+        linenumber=5;
+     System.out.println(linenumber+"나의 눈은 열려 있었고,");
+        linenumber=6;
+     System.out.println(linenumber+"나는 내가 어디 있는지 알았지.");
+        linenumber=7;
+     System.out.println(linenumber+"");
+        linenumber=8;
+     System.out.println(linenumber+"나는 동일한 길을 걷고 있었네.");
+        linenumber=9;
+     System.out.println(linenumber+"보도에는 깊은 구멍이 있었지.");
+        linenumber=10;
+     System.out.println(linenumber+"나는 그것을 피해 돌아갔지.");
+        linenumber=11;
+     System.out.println(linenumber+"");
+        linenumber=12;
+     System.out.println(linenumber+"나는 다른 길로 이제는 다니네.");
+        linenumber=13;
+     System.out.println(linenumber+"");
+        linenumber=14;
+     System.out.println(linenumber+"- 포티아 넬슨");
+	}
+
+}
---
+++ 정지수_201702073_W01/src/정지수_201702073_W01/정지수_201702073_W01_homework_03.java
@@ -0,0 +1,30 @@
+package 정지수_201702073_W01;
+
+public class 정지수_201702073_W01_homework_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int linenumber;
+		linenumber=1;
+        System.out.println(linenumber+"당신에게");
+        linenumber=2;
+        System.out.println(linenumber+"");
+        linenumber=3;
+        System.out.println(linenumber+"오늘도 당신의 밤하늘을 위해");
+        linenumber=4;
+        System.out.println(linenumber+"나의 작은 등불을 끄겠습니다.");
+        linenumber=5;
+        System.out.println(linenumber+"");
+        linenumber=6;
+        System.out.println(linenumber+"오늘도 당신의 별들을 위해");
+        linenumber=7;
+        System.out.println(linenumber+"나의 작은 촛불을 끄겠습니다.");
+        linenumber=8;
+        System.out.println(linenumber+"");
+        linenumber=9;
+        System.out.println(linenumber+"-정호승");
+	}
+
+}
+
+
